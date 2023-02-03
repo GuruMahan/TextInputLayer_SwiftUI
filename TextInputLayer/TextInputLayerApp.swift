ce0ef965d88cb011cda7d54407bf109842ad8a1a
GuruMahan <iosdeve2022@gmail.com>
2023-02-03 10:25:57 +0530

    Initial Commit

---
+++ TextInputLayer/TextInputLayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextInputLayerApp.swift
+//  TextInputLayer
+//
+//  Created by Guru Mahan on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TextInputLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
